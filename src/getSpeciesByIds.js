e7bdaadd6a1701ef7dd9ca83afee967470b659f0
Victoria Teixeira <victoriafteixeira@hotmail.com>
2022-08-29 14:47:11 +1000

    corrigindo erros de linter

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,5 +1,5 @@
 const { species } = require('../data/zoo_data');
-const zoo_data = require('../data/zoo_data');
+// const zoo_data = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
