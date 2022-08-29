99557caae7f2a1fbbb1da2ab8fc5a0fde82beed9
Victoria Teixeira <victoriafteixeira@hotmail.com>
2022-08-29 15:45:31 +1000

    resolve o requisito 5 do projeto

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const allAnimals = {};
+    species.forEach((element) => {
+      allAnimals[element.name] = element.residents.length;
+    });
+    return allAnimals;
+  }
+  if (animal.sex === undefined) {
+    return species.find((element) => element.name === animal.specie).residents.length;
+  }
+  const searchedAnimal = species.find((element) => element.name === animal.specie);
+  return searchedAnimal.residents.filter((resident) => resident.sex === animal.sex).length;
+  // return searchedAnimal.residents.length;
 }
+console.log(countAnimals({ specie: 'bears', sex: 'female' }));
 
 module.exports = countAnimals;
