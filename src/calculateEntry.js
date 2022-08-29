5a452e5c892b8b778f563b22795ad0d7803e484d
Victoria Teixeira <victoriafteixeira@hotmail.com>
2022-08-29 21:41:06 +1000

    resolve o requisito 8 do projeto

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,20 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const ageDistribution = {
+    child: entrants.filter((child) => child.age < 18).length,
+    adult: entrants.filter((adult) => adult.age >= 18 && adult.age < 50).length,
+    senior: entrants.filter((senior) => senior.age >= 50).length,
+  };
+  return ageDistribution;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) return 0;
+  const entrantsByAge = countEntrants(entrants);
+  return Object.keys(entrantsByAge)
+    .reduce((acc, curr) => acc + (entrantsByAge[curr] * prices[curr]), 0);
 }
-
+console.log(calculateEntry({}));
 module.exports = { calculateEntry, countEntrants };
