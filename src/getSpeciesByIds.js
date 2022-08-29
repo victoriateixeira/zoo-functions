84066e0f50890d196ff08fa05390dcb73d7c6871
Victoria Teixeira <victoriafteixeira@hotmail.com>
2022-08-29 10:35:42 +1000

    resolve o requisito 1 do projeto

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
+const { species } = require('../data/zoo_data');
+const zoo_data = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) return [];
+  return species.filter((animal) => ids.includes(animal.id));
 }
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
