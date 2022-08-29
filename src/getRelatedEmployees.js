3f739bd5f9383989fe46ef64350c7d9b6ee58d7b
Victoria Teixeira <victoriafteixeira@hotmail.com>
2022-08-29 14:44:18 +1000

    resolve o requisito 4 do projeto

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+
 function isManager(id) {
-  // seu código aqui
+  const searchedEmployee = employees.find((employee) => employee.id === id);
+  return searchedEmployee.managers.length === 0 || searchedEmployee.managers.includes(stephanieId);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return employees.filter((employee) => employee.managers
+    .includes(managerId))
+    .map((employee) => `${employee.firstName} ${employee.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
